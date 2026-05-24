bff641e4a6dd478a4670f4ca0951ffb292f9c5c3
ruksat13 <mdruksathasanakib@gmail.com>
2026-05-24 14:45:11 +0600

    Fix serverless MongoDB connection for Vercel

--- index.js
+++ index.js
@@ -18,90 +18,86 @@ const client = new MongoClient(uri, {
     },
 })
 
-async function run() {
-    try {
+let isConnected = false
+
+const connectDB = async () => {
+    if (!isConnected) {
         await client.connect()
-
-        const db = client.db('rentwheels')
-        const carsCollection = db.collection('cars')
-        const bookingsCollection = db.collection('bookings')
-
-        app.get('/', (req, res) => {
-            res.send('RentWheels Server is Running!')
-        })
-
-        app.get('/cars', async (req, res) => {
-            const result = await carsCollection.find().sort({ _id: -1 }).toArray()
-            res.send(result)
-        })
-
-        app.get('/cars/featured', async (req, res) => {
-            const result = await carsCollection.find().sort({ _id: -1 }).limit(6).toArray()
-            res.send(result)
-        })
-
-        app.get('/cars/:id', async (req, res) => {
-            const id = req.params.id
-            const query = { _id: new ObjectId(id) }
-            const result = await carsCollection.findOne(query)
-            res.send(result)
-        })
-
-        app.get('/my-cars', async (req, res) => {
-            const email = req.query.email
-            const query = { providerEmail: email }
-            const result = await carsCollection.find(query).toArray()
-            res.send(result)
-        })
-
-        app.post('/cars', async (req, res) => {
-            const car = req.body
-            const result = await carsCollection.insertOne(car)
-            res.send(result)
-        })
-
-        app.put('/cars/:id', async (req, res) => {
-            const id = req.params.id
-            const car = req.body
-            const filter = { _id: new ObjectId(id) }
-            const updated = { $set: car }
-            const result = await carsCollection.updateOne(filter, updated)
-            res.send(result)
-        })
-
-        app.delete('/cars/:id', async (req, res) => {
-            const id = req.params.id
-            const query = { _id: new ObjectId(id) }
-            const result = await carsCollection.deleteOne(query)
-            res.send(result)
-        })
-
-        app.post('/bookings', async (req, res) => {
-            const booking = req.body
-            const existing = await bookingsCollection.findOne({ carId: booking.carId })
-            if (existing) {
-                return res.send({ alreadyBooked: true })
-            }
-            const result = await bookingsCollection.insertOne(booking)
-            await carsCollection.updateOne(
-                { _id: new ObjectId(booking.carId) },
-                { $set: { status: 'booked' } }
-            )
-            res.send(result)
-        })
-
-        app.get('/bookings', async (req, res) => {
-            const email = req.query.email
-            const query = { userEmail: email }
-            const result = await bookingsCollection.find(query).toArray()
-            res.send(result)
-        })
-
-    } catch (err) {
-        console.error(err)
+        isConnected = true
     }
+    return client.db('rentwheels')
 }
 
-run()
+app.get('/', async (req, res) => {
+    res.send('RentWheels Server is Running!')
+})
+
+app.get('/cars/featured', async (req, res) => {
+    const db = await connectDB()
+    const result = await db.collection('cars').find().sort({ _id: -1 }).limit(6).toArray()
+    res.send(result)
+})
+
+app.get('/cars', async (req, res) => {
+    const db = await connectDB()
+    const result = await db.collection('cars').find().sort({ _id: -1 }).toArray()
+    res.send(result)
+})
+
+app.get('/my-cars', async (req, res) => {
+    const db = await connectDB()
+    const email = req.query.email
+    const result = await db.collection('cars').find({ providerEmail: email }).toArray()
+    res.send(result)
+})
+
+app.get('/cars/:id', async (req, res) => {
+    const db = await connectDB()
+    const result = await db.collection('cars').findOne({ _id: new ObjectId(req.params.id) })
+    res.send(result)
+})
+
+app.post('/cars', async (req, res) => {
+    const db = await connectDB()
+    const result = await db.collection('cars').insertOne(req.body)
+    res.send(result)
+})
+
+app.put('/cars/:id', async (req, res) => {
+    const db = await connectDB()
+    const result = await db.collection('cars').updateOne(
+        { _id: new ObjectId(req.params.id) },
+        { $set: req.body }
+    )
+    res.send(result)
+})
+
+app.delete('/cars/:id', async (req, res) => {
+    const db = await connectDB()
+    const result = await db.collection('cars').deleteOne({ _id: new ObjectId(req.params.id) })
+    res.send(result)
+})
+
+app.post('/bookings', async (req, res) => {
+    const db = await connectDB()
+    const booking = req.body
+    const existing = await db.collection('bookings').findOne({ carId: booking.carId })
+    if (existing) {
+        return res.send({ alreadyBooked: true })
+    }
+    const result = await db.collection('bookings').insertOne(booking)
+    await db.collection('cars').updateOne(
+        { _id: new ObjectId(booking.carId) },
+        { $set: { status: 'booked' } }
+    )
+    res.send(result)
+})
+
+app.get('/bookings', async (req, res) => {
+    const db = await connectDB()
+    const email = req.query.email
+    const result = await db.collection('bookings').find({ userEmail: email }).toArray()
+    res.send(result)
+})
 
 module.exports = app
